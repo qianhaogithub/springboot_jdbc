7090e6400993632eddef6e414900ef99fb7edbd1
qianhao <1061000328@qq.com>
2020-05-30 14:15:45 +0800

    springjdbc-01

---
+++ src/main/java/com/example/springjdbc_test/SpringjdbcTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjdbc_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjdbcTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringjdbcTestApplication.class, args);
+    }
+
+}
